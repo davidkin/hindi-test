66f2fa5adc735f63246d07149785efe99a8c5eca
David Potapkin <davidosimo1@gmail.com>
2020-06-03 11:04:38 +0300

    Time: add time zone

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import * as moment from 'moment-timezone';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +10,13 @@ import { TranslateService } from '@ngx-translate/core';
 export class HeaderComponent implements OnInit {
   @Input() translate: TranslateService;
 
+  usaTime = moment.tz('America/New_York').format();
+
   constructor() { }
 
   ngOnInit() {
+    console.log('England:', this.usaTime);
+
   }
 
 }
